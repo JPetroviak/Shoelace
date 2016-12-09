f8ffff7bdba91ace595857c6db184b2a3217b9d7
John Petroviak <johnpetroviak@John-Petroviak.local>
2016-12-09 14:50:11 -0600

    Slideshow Updates

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,13 +2,13 @@
 function changeSlide(slides) {
 	var slideActive = slides.children('.slide.active'),
 		 slideActiveDuration = slideActive.data('duration'),
-		 slideActiveTransitionSpeed = slideActive.data('speed');	
+		 slideActiveTransitionSpeed = slideActive.data('transition-speed');	
 	if (slideActive.next('.slide').length) {
 		var slideNext = slideActive.next('.slide');
 	} else {
 		var slideNext = slides.children('.slide:first-child');
 	}
-	var slideNextTransitionSpeed = slideNext.data('speed'),
+	var slideNextTransitionSpeed = slideNext.data('transition-speed'),
 		 timer = setTimeout(function() {
 			 slideActive.css('transition-duration', slideNextTransitionSpeed + 'ms').removeClass('active').addClass('previous');
 			 setTimeout(function() {
