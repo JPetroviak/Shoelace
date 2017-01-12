0c36b9b59ade2bdac4d4b93437102fd49e17c73c
John Petroviak <johnpetroviak@John-Petroviak.local>
2017-01-12 12:07:59 -0600

    Texture Limiter

---
+++ js/textarealimiter.js
@@ -0,0 +1,23 @@
+// Textarea Limiter
+$('textarea').each(function() {
+  var textarea = $(this),
+      type = textarea.attr('data-type'),
+      limit = textarea.attr('data-limit'),
+      helper = textarea.next('.input-helper');
+  helper.html(limit + ' ' + type + ' remaining.');
+  textarea.keydown(function(event) {
+    if (type == 'words') {
+      var count = textarea.val().split(/[\s]+/).length;
+    } else if (type == 'characters') {
+      var count = textarea.val().length;
+    }
+    if (count > limit) { 
+      if (event.keyCode == 46 || event.keyCode == 8) {
+      } else if (event.keyCode < 48 || event.keyCode > 57) {
+        return false;
+      }
+    }
+    var remaining = limit - count;
+    helper.html(remaining + ' ' + type + ' remaining.');
+  });
+});
