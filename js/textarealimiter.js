aa097d87f24dfaf738a988aa7a5a3cab5b982a1f
John Petroviak <johnpetroviak@John-Petroviak.local>
2017-01-12 12:49:45 -0600

    Textarea Limiter Bug Fix

--- js/textarealimiter.js
+++ js/textarealimiter.js
@@ -5,17 +5,24 @@ $('textarea').each(function() {
       limit = textarea.attr('data-limit'),
       helper = textarea.next('.input-helper');
   helper.html(limit + ' ' + type + ' remaining.');
-  textarea.keydown(function(event) {
+  textarea.on('keydown', function(event) {
     if (type == 'words') {
-      var count = textarea.val().split(/[\s]+/).length;
+      var count = textarea.val().split(/[\s]+/).length - 1;
     } else if (type == 'characters') {
       var count = textarea.val().length;
     }
-    if (count > limit) { 
+    if (count >= limit) { 
       if (event.keyCode == 46 || event.keyCode == 8) {
       } else if (event.keyCode < 48 || event.keyCode > 57) {
         return false;
       }
+    }
+  });
+  textarea.on('keyup', function() {
+    if (type == 'words') {
+      var count = textarea.val().split(/[\s]+/).length - 1;
+    } else if (type == 'characters') {
+      var count = textarea.val().length;
     }
     var remaining = limit - count;
     helper.html(remaining + ' ' + type + ' remaining.');
