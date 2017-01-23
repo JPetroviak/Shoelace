3f8d6690788018bc5059f18b41b1d99fdb1a2a18
John Petroviak <johnpetroviak@John-Petroviak.local>
2017-01-23 15:10:14 -0600

    Textarea Limiter Updated

--- js/textarealimiter.js
+++ js/textarealimiter.js
@@ -1,28 +1,23 @@
 // Textarea Limiter
-$('textarea').each(function() {
+$('textarea[data-action="limiter"]').each(function() {
   var textarea = $(this),
       type = textarea.attr('data-type'),
       limit = textarea.attr('data-limit'),
       helper = textarea.next('.input-helper');
   helper.html(limit + ' ' + type + ' remaining.');
-  textarea.on('keydown', function(event) {
+  textarea.on('keydown keyup change focus blur paste', function(event) {
     if (type == 'words') {
       var count = textarea.val().split(/[\s]+/).length - 1;
     } else if (type == 'characters') {
       var count = textarea.val().length;
     }
-    if (count >= limit) { 
-      if (event.keyCode == 46 || event.keyCode == 8) {
-      } else if (event.keyCode < 48 || event.keyCode > 57) {
-        return false;
+    if (event.type == 'keydown') {
+      if (count >= limit) { 
+        if (event.keyCode == 46 || event.keyCode == 8) {
+        } else if (event.keyCode < 48 || event.keyCode > 57) {
+          return false;
+        }
       }
-    }
-  });
-  textarea.on('keyup', function() {
-    if (type == 'words') {
-      var count = textarea.val().split(/[\s]+/).length - 1;
-    } else if (type == 'characters') {
-      var count = textarea.val().length;
     }
     var remaining = limit - count;
     helper.html(remaining + ' ' + type + ' remaining.');
